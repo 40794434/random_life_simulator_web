03ac2eb4bd806d1892f68594ffdf7e6935a10425
40794434 <40794434@live.napier.ac.uk>
2026-04-25 11:27:03 +0630

    about.js

---
+++ js/about.js
@@ -0,0 +1,7 @@
+document.getElementById("backHome").addEventListener("click", function(){
+    window.location.href = "index.html";
+})
+
+document.getElementById("goCreate").addEventListener("click", function(){
+    window.location.href = "create.html";
+})
